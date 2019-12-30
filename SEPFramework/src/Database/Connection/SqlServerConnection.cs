8dca324239363e1aa8e9d467ba83b76691ed4840
devilleo <lexuankha2409@gmail.com>
2019-12-30 12:53:51 +0700

    add sqlserver connection

--- SEPFramework/src/Database/Connection/SqlServerConnection.cs
+++ SEPFramework/src/Database/Connection/SqlServerConnection.cs
@@ -1,28 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace SEPFramework
 {
-    public class SqlServerConnection : CommonConnection
+    public class SqlServer : CommonConnection
     {
-        public SqlServerConnection(string host, string dbname, string username, string pass, int port) : base(host, dbname, username, pass, port)
+        SqlConnection connection = new SqlConnection();
+        public SqlServer(string host, string dbname, string username, string pass, int port) : base(host, dbname, username, pass, port)
         {
         }
 
         public override bool Connect()
         {
-            throw new NotImplementedException();
+            SqlConnectionStringBuilder connBuilder = new SqlConnectionStringBuilder();
+            connBuilder["Data Source"] = host;
+            if (username != "" && pass != "")
+            {
+                connBuilder["integrated Security"] = false;
+                connBuilder["Password"] = pass;
+                connBuilder["UserID"] = username;
+            }
+            else
+            {
+                connBuilder["integrated Security"] = true;
+            }
+            connBuilder["Initial Catalog"] = dbname;
+            connection = new SqlConnection(connBuilder.ConnectionString);
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
         }
 
         public override List<string> getListTableName()
         {
-            throw new NotImplementedException();
+            DataTable schema = connection.GetSchema("Tables");
+            List<string> TableNames = new List<string>();
+            foreach (DataRow row in schema.Rows)
+            {
+                TableNames.Add(row[2].ToString());
+            }
+            return TableNames;
         }
 
         public override DataTable getTable(string tableName)
         {
-            throw new NotImplementedException();
+            var stm = "SELECT * FROM " + tableName;
+            DataTable tb = new DataTable();
+            var cmd = new SqlCommand(stm, connection);
+            SqlDataReader rdr = cmd.ExecuteReader();
+            if (rdr.HasRows)
+            {
+                tb.Load(rdr);
+            }
+
+            rdr.Close();
+            return tb;
         }
     }
 }
