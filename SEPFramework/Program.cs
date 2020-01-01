96a4b3188b62c481611e2213c145c08070cb95fa
wedday2004 <48178473+wedday2004@users.noreply.github.com>
2020-01-01 12:53:24 +0700

    reponsize UI

--- SEPFramework/Program.cs
+++ SEPFramework/Program.cs
@@ -13,7 +13,7 @@ namespace SEPFramework
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            CommonConnection connection = ConnectionFactory.createConnection("mysql","remotemysql.com", "WEJMD9dLmJ", "WEJMD9dLmJ", "CqlKK8zDL3", 3306);
+            CommonConnection connection = ConnectionFactory.createConnection("mysql","remotemysql.com", "6cjHslMrqJ", "6cjHslMrqJ", "gkYsnqNLTV", 3306);
 
             ////CommonConnection connection =  ConnectionFactory.createConnection("sqlserver",@"DESKTOP-FRPO8I4\SQLEXPRESS", "testDB", "dffd", "sfd", 1433);
             //CommonConnection connection =  ConnectionFactory.createConnection("sqlserver",@"DESKTOP-FRPO8I4\SQLEXPRESS", "testDB", "", "", 1433);
